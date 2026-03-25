5ca6ddf8c3de0b1994f201ca9537d1561c18b5da
Odell <odell8933@gmail.com>
2026-03-25 10:06:45 -0500

    Allow PATCH requests for profile updates

--- server.js
+++ server.js
@@ -1263,7 +1263,7 @@ app.use((req, res, next) => {
     }
 
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PATCH,OPTIONS");
 
     if (req.method === "OPTIONS") {
         return res.sendStatus(204);
